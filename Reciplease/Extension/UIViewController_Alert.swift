0f0c1eb8570baa2e6d4d941ff41172138e21f191
yoan8306 <yoan8306@wanadoo.fr>
2022-05-02 23:58:30 +0200

    add segues to recipesList create model for add ingredients list

---
+++ Reciplease/Extension/UIViewController_Alert.swift
@@ -0,0 +1,20 @@
+//
+//  UIViewController_Alert.swift
+//  Reciplease
+//
+//  Created by Yoan on 02/05/2022.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func presentAlert (alertTitle title: String = "Error", alertMessage message: String,
+                       buttonTitle titleButton: String = "Ok",
+                       alertStyle style: UIAlertAction.Style = .cancel ) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: titleButton, style: style, handler: nil)
+        alertVC.addAction(action)
+        present(alertVC, animated: true, completion: nil)
+    }
+}
--- Reciplease/Model/IngredientsList.swift
+++ Reciplease/Model/IngredientsList.swift
@@ -9,13 +9,44 @@ import Foundation
 
 class IngredientsList {
     var listIngredient: [String]
-    
+
     init() {
         listIngredient = []
     }
+
+// MARK: - Add ingredient
+    func addIngredient(ingredientList: String) -> Bool {
+        let myList = deleteSpace(ingredientList: ingredientList)
+        guard checkListIngredient(freedSpaceString: myList) else {
+            return false
+        }
+        listIngredient.append(contentsOf: myList.components(separatedBy: ",") )
+        return true
+    }
     
-    func addIngredient(ingredientList: String) {
+    private func deleteSpace(ingredientList: String) -> String {
         let freedSpaceString = ingredientList.filter {!$0.isWhitespace}
-        listIngredient.append(contentsOf: freedSpaceString.components(separatedBy: ",") )
+        return freedSpaceString
+    }
+    
+   private func checkListIngredient(freedSpaceString: String) -> Bool {
+       guard !freedSpaceString.isEmpty else {
+           return false
+       }
+       return true
+    }
+    
+// MARK: - delete ingredient
+    func clearListIngredient() {
+        listIngredient.removeAll()
+    }
+
+    func removeIngredient(index: Int) {
+        listIngredient.remove(at: index)
+    }
+
+// MARK: - return ingredient list
+    func returnIngredientList() -> String {
+        return listIngredient.joined(separator: ", ")
     }
 }
