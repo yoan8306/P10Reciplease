17fe2f55cc7c5b5e8bed2db20edee86843530fe1
yoan8306 <yoan8306@wanadoo.fr>
2022-05-19 10:42:03 +0200

    CreateSessionTask for mock session and modify recipeService and imageService

---
+++ Reciplease/Service/SessionTask.swift
@@ -0,0 +1,31 @@
+//
+//  SessionTask.swift
+//  Reciplease
+//
+//  Created by Yoan on 18/05/2022.
+//
+
+import Foundation
+import Alamofire
+
+protocol SessionTaskProtocol {
+    func sendTask(url: String, callBack: @escaping (Result<Data, Error>) -> Void)
+}
+
+class SessionTask: SessionTaskProtocol {
+    static let shared = SessionTask()
+    var session = Session()
+    private init() {}
+    
+    func sendTask(url: String, callBack: @escaping (Result<Data, Error>) -> Void) {
+        session.request(url)
+            .validate(statusCode: 200..<400)
+            .response(queue: DispatchQueue.global(qos: .background), completionHandler: { (response) in
+                guard let data = response.data else {
+                    callBack(.failure(response.error ?? APIError.noData))
+                    return
+                }
+                callBack(.success(data))
+            })
+    }
+}
